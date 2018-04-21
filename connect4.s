e9f6a72598cfe260f49ee85c9fc69546debd01c8
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-04-21 16:36:00 -0600

    test

--- connect4.s
+++ connect4.s
@@ -10,6 +10,7 @@ File contains subroutines for the connect 4 game.
 .align 2
 @Message strings
 inputMessage: .asciz "Judador %d: Ingrese el numero de columna (1, 2, 3, 4): \n"
+inputError: .asciz "** Valor invalido. **\n"
 @Input formats
 inputColumn: .asciz "%d"
 @Saved data
@@ -25,15 +26,19 @@ currentColumn: .word 0
  */
  .global input
  input:
+ 	player .req r5 @player variable
  	push {lr} @Save the link register
- 	mov r1, r0 @Move the param value to r1
+ 	mov player, r0 @mov the param to variable player
  	ldr r0, =inputMessage @Load input message
+ 	mov r1, player @Load the player
 	bl printf @Display message
 	ldr r0, =inputColumn @load input format
 	ldr r1, =currentColumn @Load addres to store input
 	bl scanf @Store the inpur
 	ldr r0, =currentColumn @Load input adress
 	ldr r0, [r0] @Load input value
+
+	.unreq player @Unlink the player variable from r5
 	pop {lr} @Retrieve link register
 	mov pc, lr @Return r0
 
