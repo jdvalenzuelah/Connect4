3c0740a1627fa78d80e60d426279679ab33d65a2
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-04-23 06:29:08 -0600

    Test

---
+++ Test/connect4.s
@@ -0,0 +1,374 @@
+/**********************************************************************
+Universidad del Valle de Guatemala
+Authors: Josue David Valenzuela 171001
+		 Marcos Gutierrez	    17699
+Two player connect 4 game developed as part of CC3055 course
+File contains subroutines and stores the data for the connect 4 game.
+**********************************************************************/
+
+.data
+.align 2
+@Message strings
+inputMessage: .asciz "Judador %d: Ingrese el numero de columna (1, 2, 3, 4): \n"
+inputErrorM: .asciz "** Valor invalido. **\n"
+vectorFull: .asciz "** Columna llena! ***\n"
+@Input formats
+inputColumn: .asciz "%d"
+matrix: .asciz "|%d|"
+enter: .asciz "\n"
+@Saved data
+currentColumn: .word 0
+column1: .word 0,0,0,0
+column2: .word 0,0,0,0
+column3: .word 0,0,0,0
+column4: .word 0,0,0,0
+
+.text
+.align 2
+
+/**
+ * Player input, subroutine verifys that the value is between 1 and 4 and returns the value
+ * Param r0: player number (1 or 2)
+ * Return: column number on r0
+ */
+ .global input
+ input:
+ 	player .req r5 @player variable
+ 	column .req r6 @column variable
+ 	push {lr} @Save the link register
+ 	mov player, r0 @mov the param to variable player
+ inputBody:
+ 	ldr r0, =inputMessage @Load input message
+ 	mov r1, player @Load the player
+	bl printf @Display message
+	ldr r0, =inputColumn @load input format
+	ldr r1, =currentColumn @Load addres to store input
+	bl scanf @Store the input
+	ldr r0, =currentColumn @Load input adress
+	ldr column, [r0] @Load input value
+	cmp column, #1 @if(column < 1)
+	blt inputError @True, go to input error
+	cmp column, #4 @else if(column > 4)
+	bgt inputError @True, go to input error
+	ble inputFinish @else go to input finish
+inputError:
+	ldr r0, =inputErrorM @Load error message
+	bl printf @Display error message
+	b inputBody @Go back to input inputBody
+inputFinish:
+	mov r0, column @mov the column value to r0
+	.unreq player @Unlink the player variable from r5
+	.unreq column @Unlink the cplumn variable from r6
+	pop {lr} @Retrieve link register
+	mov pc, lr @Return r0
+
+/**
+ * Insert the players input
+ * Param: r0: player number (1 or 2)
+ *        r1: column number
+ * Return: r0: vector with the players input
+*/
+.global insertInput
+insertInput:
+	column .req r5 @Variable column
+	box	   .req r6 @variable box
+	player .req r7 @variable player
+	count  .req r8 @variable count
+	columnInput .req r9 @variable columnInput
+	push {lr} @store the link register
+	mov player, r0 @store he player
+	mov column, r1 @store the column
+	cmp column, #1 @if(column == 1)
+	ldreq column, =column1 @true: load column1
+	cmp column, #2 @else if(column == 2)
+	ldreq column, =column2 @true: load column2
+	cmp column, #3 @else if(column == 3)
+	ldreq column, =column3 @true: load column3
+	cmp column, #4 @else if(column == 4)
+	ldreq column, =column4 @load column4
+	mov columnInput, column @store column input
+recorridoVector:
+	ldr box, [column] @load the current column element
+	cmp box, #0 @if element is empty
+	streq player, [column] @store the player in such element
+	beq insertInputFinish @Go to insertInputFinish
+	add column, #4 @Go to next element
+	add count, #1 @count++
+	cmp count, #4 @if count != 4
+	bne recorridoVector @true go to recorridoVector
+	ldr r0, =vectorFull @ else, load vectorFull message
+	bl printf @display vectorFull message
+insertInputFinish:
+	mov r0, columnInput @r0 = address of the vector
+	@Unlink variables from registers
+	.unreq column
+	.unreq box
+	.unreq player
+	.unreq count
+	pop {lr} @Retrieve the link register
+	mov pc, lr @return r0
+
+/**
+ * Print matrix
+ * No arguments and no return value
+*/
+.global printMatrix
+printMatrix:
+	push {lr} @store the link register
+	co1 .req r5 @co1 variable
+	co2 .req r6 @co2 variable
+	co3 .req r7 @co3 variable
+	co4 .req r8 @co4 variable
+	@load all the columns from the matrix
+	ldr co1, =column1
+	ldr co2, =column2
+	ldr co3, =column3
+	ldr co4, =column4
+	@Start from the last element
+		@Access to the second row
+	add co1, #12
+	add co2, #12
+	add co3, #12
+	add co4, #12
+	@print first row of the matrix (first row of every column)
+	ldr r0, =matrix
+	ldr r1, [co1]
+	bl printf
+	ldr r0, =matrix
+	ldr r1, [co2]
+	bl printf
+	ldr r0, =matrix
+	ldr r1, [co3]
+	bl printf
+	ldr r0, =matrix
+	ldr r1, [co4]
+	bl printf
+	ldr r0, =enter
+	bl printf
+	@Access to the second row
+	add co1, #-4
+	add co2, #-4
+	add co3, #-4
+	add co4, #-4
+	@print second row of the matrix (second row of every column)
+	ldr r0, =matrix
+	ldr r1, [co1]
+	bl printf
+	ldr r0, =matrix
+	ldr r1, [co2]
+	bl printf
+	ldr r0, =matrix
+	ldr r1, [co3]
+	bl printf
+	ldr r0, =matrix
+	ldr r1, [co4]
+	bl printf
+	ldr r0, =enter
+	bl printf
+	@Access to the third row
+	add co1, #-4
+	add co2, #-4
+	add co3, #-4
+	add co4, #-4
+	@print third row of the matrix (third row of every column)
+	ldr r0, =matrix
+	ldr r1, [co1]
+	bl printf
+	ldr r0, =matrix
+	ldr r1, [co2]
+	bl printf
+	ldr r0, =matrix
+	ldr r1, [co3]
+	bl printf
+	ldr r0, =matrix
+	ldr r1, [co4]
+	bl printf
+	ldr r0, =enter
+	bl printf
+	@Access to the fourth row
+	add co1, #-4
+	add co2, #-4
+	add co3, #-4
+	add co4, #-4
+	@print fourth row of the matrix (fourth row of every column)
+	ldr r0, =matrix
+	ldr r1, [co1]
+	bl printf
+	ldr r0, =matrix
+	ldr r1, [co2]
+	bl printf
+	ldr r0, =matrix
+	ldr r1, [co3]
+	bl printf
+	ldr r0, =matrix
+	ldr r1, [co4]
+	bl printf
+	ldr r0, =enter
+	bl printf
+	.unreq co1
+	.unreq co2
+	.unreq co3
+	.unreq co4
+	pop {lr}
+	mov pc, lr
+
+/**
+ * Verify winner
+ * Return: winner on r0,  0 for no winner yet, 1 for player 1, 2 for player 2
+*/
+.global getWinner
+getWinner:
+	co1 .req r2 @co1 variable
+	co2 .req r3 @co2 variable
+	co3 .req r4 @co3 variable
+	co4 .req r5 @co4 variable
+	@variable for each value
+	value1 .req r6
+	value2 .req r7
+	value3 .req r8
+	value4 .req r9 
+	@variable for result
+	result .req r10
+	cont .req r11
+	@Load columns from the Matrix
+	ldr co1, =column1
+	ldr co2, =column2
+	ldr co3, =column3
+	ldr co4, =column4
+	mov cont, #0
+verifyHorizontal:
+	@Load each value
+	ldr value1, [co1]
+	ldr value2, [co2]
+	ldr value3, [co3]
+	ldr value4, [co4]
+	rsb value1, value1, value2 @value1 = value1 - value2
+	rsb value3, value3, value4 @value3 = value3 - value4
+	add value1, value1, value3 @value1 = value1 + value3
+	cmp value1, #0 @If valuue1 == 0,then it means all the values are equal
+	moveq result, value2 @store the result if all are equal
+	beq verifyFinish @Go to verifyFinish
+	@go to the next element
+	add co1, #4
+	add co2, #4
+	add co3, #4
+	add co4, #4
+	add cont, #1
+	cmp cont, #4 @verify that cont != 4
+	bne verifyHorizontal @cicle on verifyHorizontal
+	@Reload all the values
+	ldr co1, =column1
+	ldr co2, =column2
+	ldr co3, =column3
+	ldr co4, =column4
+verifyvertical:
+	@load value from each value from the current column
+	ldr value1, [co1]
+	add co1, #4
+	ldr value2, [co1]
+	add co1, #4
+	ldr value3, [co1]
+	add co1, #4
+	ldr value4, [co1]
+	rsb value1, value1, value2 @value1 = value1 - value2
+	rsb value3, value3, value4 @value3 = value3 - value4
+	add value1, value1, value3 @value1 = value1 + value3
+	cmp value1, #0 @If valuue1 == 0,then it means all the values are equal
+	moveq result, value2 @store the result if all are equal
+	beq verifyFinish @Go to verifyFinish
+	@load value from each value from the current column
+	ldr value1, [co2]
+	add co1, #4
+	ldr value2, [co2]
+	add co1, #4
+	ldr value3, [co2]
+	add co1, #4
+	ldr value4, [co2]
+	rsb value1, value1, value2 @value1 = value1 - value2
+	rsb value3, value3, value4 @value3 = value3 - value4
+	add value1, value1, value3 @value1 = value1 + value3
+	cmp value1, #0 @If valuue1 == 0,then it means all the values are equal
+	moveq result, value2 @store the result if all are equal
+	beq verifyFinish @Go to verifyFinish
+	@load value from each value from the current column
+	ldr value1, [co3]
+	add co1, #4
+	ldr value2, [co3]
+	add co1, #4
+	ldr value3, [co3]
+	add co1, #4
+	ldr value4, [co3]
+	rsb value1, value1, value2 @value1 = value1 - value2
+	rsb value3, value3, value4 @value3 = value3 - value4
+	add value1, value1, value3 @value1 = value1 + value3
+	cmp value1, #0 @If valuue1 == 0,then it means all the values are equal
+	moveq result, value2 @store the result if all are equal
+	beq verifyFinish @Go to verifyFinish
+	@load value from each value from the current column
+	ldr value1, [co4]
+	add co1, #4
+	ldr value2, [co4]
+	add co1, #4
+	ldr value3, [co4]
+	add co1, #4
+	ldr value4, [co4]
+	rsb value1, value1, value2 @value1 = value1 - value2
+	rsb value3, value3, value4 @value3 = value3 - value4
+	add value1, value1, value3 @value1 = value1 + value3
+	cmp value1, #0 @If valuue1 == 0,then it means all the values are equal
+	moveq result, value2 @store the result if all are equal
+	beq verifyFinish @Go to verifyFinish
+	@Reload all the values
+	ldr co1, =column1
+	ldr co2, =column2
+	ldr co3, =column3
+	ldr co4, =column4
+verifyDiagonals:
+	@load the values from the diagonal
+	ldr value1, [co1]
+	add co2, #4
+	ldr value2, [co2]
+	add co3, #8
+	ldr value3, [co3]
+	add co4, #12
+	ldr value4, [co4]
+	rsb value1, value1, value2 @value1 = value1 - value2
+	rsb value3, value3, value4 @value3 = value3 - value4
+	add value1, value1, value3 @value1 = value1 + value3
+	cmp value1, #0 @If valuue1 == 0,then it means all the values are equal
+	moveq result, value2 @store the result if all are equal
+	beq verifyFinish @Go to verifyFinish
+	@Reload all the values
+	ldr co1, =column1
+	ldr co2, =column2
+	ldr co3, =column3
+	ldr co4, =column4
+	add co1, #12
+	ldr value1, [co1]
+	add co2, #8
+	ldr value2, [co2]
+	add co3, #4
+	ldr value3, [co3]
+	ldr value4, [co4]
+	rsb value1, value1, value2 @value1 = value1 - value2
+	rsb value3, value3, value4 @value3 = value3 - value4
+	add value1, value1, value3 @value1 = value1 + value3
+	cmp value1, #0 @If valuue1 == 0,then it means all the values are equal
+	moveq result, value2 @store the result if all are equal
+	beq verifyFinish @Go to verifyFinish
+	mov result, #0 @In case of no winner
+
+verifyFinish:
+	mov r0, result @move the result to r0
+	@Unlink all variables from registers
+	.unreq co1
+	.unreq co2
+	.unreq co3
+	.unreq co4
+	.unreq value1
+	.unreq value2
+	.unreq value3
+	.unreq value4
+	.unreq result
+	.unreq cont
+	mov pc, lr @return r0
---
+++ Test/main.s
@@ -0,0 +1,104 @@
+
+/***************************************************************
+Universidad del Valle de Guatemala
+Authors: Josue David Valenzuela 171001
+		 Marcos Gutierrez	    17699
+Two player connect 4 game developed as part of CC3055 course
+File contains the game main method for execution
+***************************************************************/
+
+/* Data used on the game */
+.data
+.align 2
+@Message strings
+welcome: .asciz "--- Bienvenidos a Cuatro en línea modificado. ---\n"
+player1: .asciz "- Jugador 1 es representado por 1.\n"
+player2: .asciz "- Jugador 2 representado por 2.\n"
+esVacio: .asciz "- Espacio vacio representado por 0.\n"
+mensajeGanador: .asciz "Ganador: ¡¡Jugador %d!!\n"
+empate: .asciz "¡Empate!"
+
+
+
+/* Game main function */
+.text
+.align 2
+.global main
+.type main,%function
+main:
+	stmfd sp!,{lr}
+
+	/* utility variables */
+	winner .req r5
+	cont .req r6
+
+
+	/* Display welcome message */
+	ldr r0, =welcome
+	bl printf
+	mov r0, #0
+
+	/* Display game instructions */
+	ldr r0, =player1
+	bl printf
+	mov r0, #0
+	ldr r0, =player2
+	bl printf
+	ldr r0, =esVacio
+	bl printf
+
+
+/* --- PLayer 1 input --- */
+player1Input:
+	mov r0, #0
+	mov r1, #0
+	mov r0, #1
+	bl input
+	@save input
+	mov r1, r0
+	mov r0, #1	
+	bl insertInput
+	@show matrix
+	bl printMatrix
+
+/* --- PLayer 2 input --- */
+player2Input:
+	mov r0, #0
+	mov r1, #0
+	mov r0, #2
+	bl input
+	@save input
+	mov r1, r0
+	mov r0, #2	
+	bl insertInput
+	@show matrix
+	bl printMatrix
+
+/* play function */
+play:
+	@Get the wineer and save it on the variable
+	bl getWinner
+	mov winner, r0
+
+	@Verify that there is a winner
+	cmp winner, #0
+	addeq cont, #1
+	beq player1Input
+
+	@Verify if there is a tie
+	cmp cont, #8
+	ldreq r0, =empate
+	ldrne r0, =mensajeGanador
+	mov r1, winner
+	bl printf
+
+	@UN LINK variables
+	.unreq winner
+	.unreq cont
+
+
+	@OS exit
+	mov r0,#0
+	mov r3,#0
+	ldmfd sp!,{lr}
+	bx lr
